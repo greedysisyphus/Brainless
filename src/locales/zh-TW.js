3f3aa357714e5bf83492f714d27333ba0742e3e9
ohnosisyphus <ohnosisyphus@gmail.com>
2024-12-03 18:04:39 +0800

    feat: add i18n support and translations

---
+++ src/locales/zh-TW.js
@@ -0,0 +1,13 @@
+export default {
+  common: {
+    loading: '載入中...',
+    error: '出錯了！',
+    retry: '重試',
+    save: '儲存',
+    cancel: '取消',
+    confirm: '確認',
+    delete: '刪除',
+    edit: '編輯'
+  },
+  // ... 其他翻譯
+} 
