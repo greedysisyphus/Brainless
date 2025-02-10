fe6ac4040dcd1dd4e8f8a762650a4f4c3d7de111
ohnosisyphus <ohnosisyphus@gmail.com>
2025-02-10 21:25:09 +0800

    S

--- src/pages/SandwichCalculator.jsx
+++ src/pages/SandwichCalculator.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 function SandwichCalculator() {
   const [settings, setSettings] = useState({
     breadPerBag: 8,
-    targetHam: 68,
-    targetSalami: 36
+    targetHam: 60,
+    targetSalami: 30
   })
   
   const [values, setValues] = useState({
